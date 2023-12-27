2d0c0c7a485cfb802e3c810a03b734812a78c0f0
dushyantbhati2 <db90054@gmail.com>
2023-12-27 09:50:30 +0530

    updated

--- social/core/views.py
+++ social/core/views.py
@@ -25,11 +25,14 @@ def signup(request):
             else:
                 user=User.objects.create_user(username=username,email=email1,password=password)
                 user.save()
-                
+                #log user in and redirect settings page
+                user_login=auth.authenticate(username=username,password=password)
+                auth.login(request,user_login)
+                #create a profile model for the user
                 user_model=User.objects.get(username=username)
                 new_profile=models.Profile.objects.create(user=user_model,id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request,'Password not matched')
             return redirect('signup')
@@ -48,7 +51,34 @@ def signin(request):
             messages.info(request,'Credentials Invalid')
             return redirect('signin')
     return render(request,'signin.html');
-
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+
+def settings(request):
+    user_profile=models.Profile.objects.get(user=request.user)
+    if request.method=="POST":
+        if request.FILES.get('image')==None:
+            image = user_profile.profimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        else:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')    
+    return render(request,'setting.html',{'user_profile':user_profile})
+
